36292e6ab4843d09163e82c93a3bc0ade7af0cd5
tuomassalomon <80488379+tuomassalomon@users.noreply.github.com>
2025-10-18 13:45:37 +0300

    Modify MongoDB URI to include user credentials
    Updated MongoDB connection URI with credentials.

--- mqtt_mongo.js
+++ mqtt_mongo.js
@@ -23,7 +23,7 @@ mq.on('connect', function(){
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 //korvaa alla oleva URI-string omalla URI:lla (hae se Mongo Atlaksen Connect-kohdasta, lisää myös oma käyttäjätunnus ja salasana)
-const uri = "mongodb+srv://@cluster0.q4jeza3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
+const uri = "mongodb+srv://tuomas:tuomas@cluster0.q4jeza3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"; 
 
 const client = new MongoClient(uri, {
   serverApi: {
